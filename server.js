2930cae71fdbc81cb29a8470ab70c1ac45d972b6
Matt Young <127060152+myoung24@users.noreply.github.com>
2023-10-12 21:40:57 -0230

    added event for 404 status code

--- server.js
+++ server.js
@@ -1,38 +1,49 @@
 const http = require("http");
 const routes = require("./routes.js");
 
+const EventEmitter = require("events");
+class myEmitter extends EventEmitter {}
+const MyEmitter = new myEmitter();
+
+const DEBUG = false;
+
+var statusCode;
+
 const server = http.createServer((request, response) => {
   let path = "./views/";
-  console.log(request.url);
+  // Use the request.url to determine in node what url was entered into the browser.
+  if (DEBUG) console.log(request.url);
+  // Use a switch, or other programming logic statement, with the request.url to determine the action to take based on the route requested.
   switch (request.url) {
     case "/":
     case "/index":
       path += "index.html";
-      console.log(path);
+      // Add a console.log() for each different case within the switch statement.
+      if (DEBUG) console.log(path);
       response.statusCode = 200;
       routes.indexPage(path, response);
       break;
     case "/about":
       path += "about.html";
-      console.log(path);
+      if (DEBUG) console.log(path);
       response.statusCode = 200;
       routes.aboutPage(path, response);
       break;
     case "/contact":
       path += "contact.html";
-      console.log(path);
+      if (DEBUG) console.log(path);
       response.statusCode = 200;
       routes.contactPage(path, response);
       break;
     case "/products":
       path += "products.html";
-      console.log(path);
+      if (DEBUG) console.log(path);
       response.statusCode = 200;
       routes.aboutPage(path, response);
       break;
     case "/subscribe":
       path += "subscribe.html";
-      console.log(path);
+      if (DEBUG) console.log(path);
       response.statusCode = 200;
       routes.aboutPage(path, response);
       break;
@@ -42,6 +53,12 @@ const server = http.createServer((request, response) => {
       routes.fourofourPage(path, response);
       break;
   }
+  statusCode = response.statusCode;
+  MyEmitter.emit(statusCode);
+});
+
+MyEmitter.on(404, () => {
+  console.log("<EVENT> statusCode:", statusCode, "page not found");
 });
 
 server.listen(3000, "localhost", () => {
